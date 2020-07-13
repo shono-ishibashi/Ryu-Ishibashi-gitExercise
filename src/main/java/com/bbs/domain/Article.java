2b6d2c584d3c6d9fe5b15087df4b050b3e5bb858
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-07-13 10:57:37 +0900

    insert-article

---
+++ src/main/java/com/bbs/domain/Article.java
@@ -0,0 +1,11 @@
+package com.bbs.domain;
+
+import lombok.Data;
+
+@Data
+public class Article {
+    private Integer id;
+    private String articleName;
+    private String articleContent;
+
+}
---
+++ src/main/java/com/bbs/form/ArticleForm.java
@@ -0,0 +1,12 @@
+package com.bbs.form;
+
+import lombok.Data;
+
+@Data
+public class ArticleForm {
+
+    private Integer id;
+    private String name;
+    private String content;
+
+}
